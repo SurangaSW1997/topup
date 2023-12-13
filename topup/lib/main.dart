76837341fa6809afc3db9728ae06d5cac976ef21
Suranga Sandaruwan <99784721+SurangaSW1997@users.noreply.github.com>
2023-12-13 18:46:59 +0530

    added images

--- topup/lib/main.dart
+++ topup/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:topup/splashpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
   }
 }
