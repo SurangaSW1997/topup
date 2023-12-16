f5de118856f2e20290b707c79e691d02e29ee6ef
Suranga Sandaruwan <99784721+SurangaSW1997@users.noreply.github.com>
2023-12-16 10:52:42 +0530

    remove unwanted packaes

--- topup/lib/main.dart
+++ topup/lib/main.dart
@@ -1,9 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-import 'package:topup/loginpage.dart';
-import 'package:topup/signuppage.dart';
 import 'package:topup/splashscreen.dart';
 
 void main() {
