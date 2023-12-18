a251baa42e6a5a4da9dbf1d0e4c50daaad5e1bdb
Suranga Sandaruwan <99784721+SurangaSW1997@users.noreply.github.com>
2023-12-18 08:26:45 +0530

    modified

--- lib/topuppage.dart
+++ lib/topuppage.dart
@@ -6,7 +6,9 @@ class TopUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(),
+      body: Row(
+        children: [],
+      ),
     );
   }
 }
