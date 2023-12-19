e8b41e0ac8d83afb9c6d41d65c9e910e9c89b445
Suranga Sandaruwan <99784721+SurangaSW1997@users.noreply.github.com>
2023-12-19 08:50:25 +0530

    install firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:topup/bottomnavbar.dart';
@@ -7,7 +8,10 @@ import 'package:topup/homepage.dart';
 
 import 'package:topup/splashscreen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const MyApp());
 }
 
@@ -16,8 +20,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    WidgetsFlutterBinding.ensureInitialized();
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
