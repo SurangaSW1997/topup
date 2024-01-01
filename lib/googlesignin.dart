4d7a3938509566e358b90f5578630dc25168aab1
Suranga Sandaruwan <99784721+SurangaSW1997@users.noreply.github.com>
2024-01-01 19:18:12 +0530

    comment

--- lib/googlesignin.dart
+++ lib/googlesignin.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+//google signin
 class GoogleSignInProvider extends ChangeNotifier {
   final googleSignIn = GoogleSignIn();
 
