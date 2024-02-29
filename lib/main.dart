49bc05ec86c72665228f59dd9cea2996f5494062
Suranga Sandaruwan <mpsurangasw1111@gmail.com>
2024-02-29 20:45:40 +0530

    remove all unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
-import 'package:topup/aboutpage.dart';
-import 'package:topup/gamepage.dart';
-import 'package:topup/homepage.dart';
-import 'package:topup/loginintropage.dart';
-import 'package:topup/loginpage.dart';
-import 'package:topup/profilepage.dart';
-import 'package:topup/signuppage.dart';
 
 import 'package:topup/splashscreen.dart';
-import 'package:topup/topuppage.dart';
 
 //mainn
 Future main() async {
